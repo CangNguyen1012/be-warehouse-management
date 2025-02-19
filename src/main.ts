f6a33bc6b6922c89d133a680726c2992d9ca4283
CangNguyen1012 <brunomessi933@gmail.com>
2025-02-19 09:26:35 +0700

    update and fixfix

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Swagger setup
-  const config = new DocumentBuilder().setTitle('Warehouse Management').setDescription('The Warehouse Management API description').setVersion('1.0').addTag('products').build();
+  const config = new DocumentBuilder()
+    .setTitle('Warehouse Management')
+    .setDescription('The Warehouse Management API description')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
