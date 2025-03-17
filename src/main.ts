29545197e983cd8c38bfd37c481ae1b58bcced89
CangNguyen <brunomessi933@gmail.com>
2025-03-17 14:05:04 +0700

    add Validation pipes

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove unknown properties
+      forbidNonWhitelisted: true, // Reject unknown properties
+      transform: true, // Automatically transform payloads to DTO instances
+    }),
+  );
 
   // Swagger setup
   const config = new DocumentBuilder()
     .setTitle('Warehouse Management')
     .setDescription('The Warehouse Management API description')
     .setVersion('1.0')
-    .addTag('products')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
