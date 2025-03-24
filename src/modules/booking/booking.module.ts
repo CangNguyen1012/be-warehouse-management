ca5d914665e9aa01afd6611401423297621518bf
CangNguyen <brunomessi933@gmail.com>
2025-03-24 17:10:55 +0700

    update booking module

--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -3,12 +3,13 @@ import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Booking, BookingSchema } from './schemas/booking.schema';
+import { BookingRepository } from './repository/booking.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Booking.name, schema: BookingSchema }]),
   ],
-  providers: [BookingService],
+  providers: [BookingService, BookingRepository],
   controllers: [BookingController],
 })
 export class BookingModule {}
